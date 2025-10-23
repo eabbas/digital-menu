ca1e5828d123b8b68fe6746ba907f61c4ea3816c
unknown <dalyn0483@gmail.com>
2025-10-23 17:43:24 +0330

    user version 01

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/login', function () {
+    return view('users.login');
+})->name('user_login');
+
+Route::get("user/signup",[UserController::class,"create"])->name('user_signUp');
+Route::post("user/store",[UserController::class,"store"]);
+Route::post("user/check",[UserController::class,"check"])->name('checkUser');
+Route::get("user/logout",[UserController::class,"logout"]);
+Route::get("user/index",[UserController::class,"index"]);
+Route::get("user/show/{id}",[UserController::class,"show"]);
+Route::get("user/edit/{id}",[UserController::class,"edit"]);
+Route::post("user/update",[UserController::class,"update"])->name('userUpdate');
+Route::get("user/delete/{id}",[UserController::class,"delete"]);
+
+
