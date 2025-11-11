36a577773fe33a961ed65bd54de5f9ddfb2d7729
unknown <dalyn0483@gmail.com>
2025-11-11 19:00:01 +0330

    aylar-slider-edit-update

---
+++ resources/views/admin/slider/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('admin.user.panel')
+@section('title', 'ویرایش اسلایدر')
+@section('content')
+            <div class="my-10">
+                <h1 class="lg:text-3xl md:text-2xl text-md font-semibold text-center text-gray-700">ویرایش اسلایدر</h1>
+            </div>
+            <form action="{{ route('slider.sliderUpdate') }}" method="post" enctype='multipart/form-data' class="w-11/12 lg:w-3/4 mx-auto p-5 rounded-lg border">
+                @csrf
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-5 lg:gap-10">
+                    <div class="w-full flex flex-col">
+                        <input type="hidden" name ="id" value ="{{$slider -> id}}">
+                        <fieldset class="text-sm md:text-base border border-gray-400 rounded-[20px] py-1 pr-3"
+                            for="slider_img">
+                            <legend class="p-1 w-20 bg-gray-200 rounded-full flex flex-row justify-center text-sm">اسلایدر
+                            </legend>
+                            <input type="file" name="slider_img" class="w-full px-2 py-1 lg:px-2 outline-none text-gray-500">
+                        </fieldset>
+                    </div>
+                    <div class="w-full flex flex-col">
+                        <label class="text-sm md:text-base" for="title">عنوان اسلایدر :</label>
+                        <input type="title" name="title"  value="{{$slider -> title}}" class="w-full px-2 py-1 lg:px-2 outline-none border-b" required>
+                    </div>
+                <div class="md:text-left text-center md:px-12 mt-5 lg:mt-10">
+                    <button class="px-5 py-2 lg:px-10 lg:py-3 border rounded-md transition-all duration-150 hover:bg-gray-400 hover:border-gray-400 hover:text-white">ثبت</button>
+                </div>
+            </form>
+@endsection
