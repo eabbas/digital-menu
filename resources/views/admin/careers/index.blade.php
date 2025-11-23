66bc64756e7a6ba23f9c91c78c6b153c86af2238
Mohammad Eftekhari <turkaslan1206@gmail.com>
2025-11-23 17:14:02 +0330

    careers and user career front debugged

--- resources/views/admin/careers/index.blade.php
+++ resources/views/admin/careers/index.blade.php
@@ -1,68 +1,96 @@
 @extends('admin.app.panel')
-    @section('title', ' همه کسب وکارها')
-    @section('content')
-
-
+@section('title', ' همه کسب وکارها')
+@section('content')
     <div class="w-full flex flex-col pb-4">
-    <div class="bg-white rounded-lg">
-        <div class="pb-4">
-            <h2 class="text-lg font-bold text-gray-800">اطلاعات کسب و کار</h2>
-        </div>
-        <div class="flex flex-col gap-5">
-            <div class="overflow-x-auto shadow-md" style="scrollbar-width: none;">
-                <div class="min-w-full bg-gray-200">
-                    <div class="bg-gray-100 grid grid-cols-8 items-center divide-x divide-[#f1f1f4]">
-                        <div class="px-6 py-3 text-center text-xs font-medium text-gray-600">عنوان</div>
-                        <div class="px-6 py-3 text-center text-xs font-medium text-gray-600 col-span-2">نام صاحب کسب و کار</div>
-                        <div class="px-6 py-3 text-center text-xs font-medium text-gray-600">توضیحات</div>
-                        <div class="px-6 py-3 text-center text-xs font-medium text-gray-600">لوگو</div>
-                        <div class="px-6 py-3 text-center text-xs font-medium text-gray-600 col-span-3">عملیات</div>
-                    </div>
-                    <div class="bg-white divide-y divide-[#f1f1f4]">
-                        @foreach($careers as $career)
-                        @if($career)
-                        <div class="grid grid-cols-8 items-center divide-x divide-[#f1f1f4]">
-                            <div class="p-3 text-sm h-full flex items-center justify-center text-gray-900">{{ $career->title}}</div>
-                            <div class="p-3 text-sm h-full flex items-center justify-center text-gray-900 col-span-2">{{ $career?->user?->name }} {{ $career?->user?->family }}</div>
-                            <div class="p-3 text-sm h-full flex items-center justify-center text-gray-900">{{ $career->description}}</div>
-                            <div class="p-3 text-sm h-full flex items-center justify-center text-gray-900">
-                                <img class="max-w-[50px] max-h-[50px] mx-auto size-12 object-cover" src="<?= asset("storage/" . $career->logo) ?>">
+        <div class="bg-white rounded-lg">
+            <div class="pb-4">
+                <h2 class="text-lg font-bold text-gray-800">اطلاعات کسب و کار</h2>
+            </div>
+            <div class="flex flex-col gap-5">
+                <div class="overflow-x-auto shadow-md" style="scrollbar-width: none;">
+                    <div class="min-w-full bg-gray-200">
+                        <div class="w-full flex flex-row lg:grid lg:grid-cols-6 items-center divide-x divide-[#f1f1f4]">
+                            <div class="px-1 lg:px-6 py-3 text-center text-xs font-medium text-gray-600 bg-gray-100">
+                                <span class="block w-20 lg:w-full">عنوان</span>
                             </div>
-                            <div class="w-full grid grid-cols-4 col-span-3 h-full divide-x divide-[#f1f1f4]">
-                                <div class="p-3 text-sm h-full flex items-center justify-center font-medium">
-                                    <a href="{{ route('career.single', [$career]) }}" class="ml-4 text-sky-700">مشاهده</a>
-                                </div>
-                                <div class="p-3 text-sm h-full flex items-center justify-center font-medium">
-                                    <a href="{{ route('career.edit', [$career, $career->user])}}" class="ml-4 text-sky-700">ویرایش</a>
-                                </div>
-                                <div class="p-3 text-sm h-full flex items-center justify-center font-medium">
-                                    <a href="{{ route('career.delete', [$career])}}" class="ml-4 text-sky-700">حذف</a>
-                                </div>
-                                @if($career->menu)
-                                <div class="p-3 text-sm h-full flex items-center justify-center font-medium">
-                                    <a href="{{ route('menu.list', [$career])}}" class="ml-4 text-sky-700">مشاهده منو</a>
-                                </div>
-                                @endif
-                                @if(!$career->menu)
-                                <div class="p-3 text-sm h-full flex items-center justify-center font-medium">
-                                    <a href="{{ route('menu.create', [$career])}}" class="ml-4 text-sky-700">ایجاد منو</a>
-                                </div>
-                                @endif
+                            <div class="px-1 lg:px-6 py-3 text-center text-xs font-medium text-gray-600 bg-gray-100">
+                                <span class="block w-24 lg:w-full">توضیحات</span>
+                            </div>
+                            <div class="px-1 lg:px-6 py-3 text-center text-xs font-medium text-gray-600 bg-gray-100">
+                                <span class="block w-20 lg:w-full">لوگو</span>
+                            </div>
+                            <div
+                                class="px-1 lg:px-6 py-3 text-center text-xs font-medium text-gray-600 bg-gray-100 col-span-3">
+                                <span class="block w-60 lg:w-full">عملیات</span>
                             </div>
                         </div>
-                        @else
-                        <div>
-                            <div class="px-6 py-4 text-center text-sm text-gray-500">
-                                هیچ اطلاعاتی یافت نشد
-                            </div>
+                        <div class="bg-white divide-y divide-[#f1f1f4]">
+                            @foreach ($careers as $career)
+                                @if ($career)
+                                    <div
+                                        class="w-full flex flex-row lg:grid lg:grid-cols-6 items-center divide-x divide-[#f1f1f4]">
+                                        <div
+                                            class="p-1 lg:p-3 text-xs lg:text-sm h-full flex items-center justify-center text-gray-900 text-center">
+                                            <span class="block w-20 lg:w-full">{{ $career->title }}</span>
+                                        </div>
+                                        <div
+                                            class="p-1 lg:p-3 text-xs lg:text-sm h-full flex items-center justify-center text-gray-900 w-[500px] lg:w-full text-center">
+                                            <span class="block w-24 lg:w-full">{{ $career->description }}</span>
+                                        </div>
+                                        <div
+                                            class="p-1 lg:p-3 text-xs lg:text-sm h-full flex items-center justify-center text-gray-900">
+                                            <div class="w-20 lg:w-full">
+                                                <img class="max-w-[50px] max-h-[50px] mx-auto size-12 object-cover"
+                                                    src="<?= asset('storage/' . $career->logo) ?>">
+                                            </div>
+                                        </div>
+                                        <div class="w-full col-span-3">
+                                            <div
+                                                class="grid grid-cols-4 h-full divide-x divide-[#f1f1f4] w-60 lg:w-full">
+                                                <div
+                                                    class="p-1 lg:p-3 text-xs text-center lg:text-sm h-full flex items-center justify-center font-medium">
+                                                    <a href="{{ route('career.single', [$career]) }}"
+                                                        class="text-sky-700">مشاهده</a>
+                                                </div>
+                                                <div
+                                                    class="p-1 lg:p-3 text-xs text-center lg:text-sm h-full flex items-center justify-center font-medium">
+                                                    <a href="{{ route('career.edit', [$career]) }}"
+                                                        class="text-sky-700">ویرایش</a>
+                                                </div>
+                                                <div
+                                                    class="p-1 lg:p-3 text-xs text-center lg:text-sm h-full flex items-center justify-center font-medium">
+                                                    <a href="{{ route('career.delete', [$career]) }}"
+                                                        class="text-sky-700">حذف</a>
+                                                </div>
+                                                @if ($career->menu)
+                                                    <div
+                                                        class="p-1 lg:p-3 text-xs text-center lg:text-sm h-full flex items-center justify-center font-medium">
+                                                        <a href="{{ route('menu.list', [$career]) }}"
+                                                            class="text-sky-700">مشاهده منو</a>
+                                                    </div>
+                                                @endif
+                                                @if (!$career->menu)
+                                                    <div
+                                                        class="p-1 lg:p-3 text-xs text-center lg:text-sm h-full flex items-center justify-center font-medium">
+                                                        <a href="{{ route('menu.create', [$career]) }}"
+                                                            class="text-sky-700">ایجاد منو</a>
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                @else
+                                    <div>
+                                        <div class="px-1 lg:px-6 py-4 text-center text-xs lg:text-sm text-gray-500">
+                                            هیچ اطلاعاتی یافت نشد
+                                        </div>
+                                    </div>
+                                @endif
+                            @endforeach
                         </div>
-                        @endif
-                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-    @endsection
+@endsection
