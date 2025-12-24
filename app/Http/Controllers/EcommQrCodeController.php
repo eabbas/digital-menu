37cdeb2c94943ba08d515d8e9b87bee6c79555bd
mobina <mobinanaghizadeh4@gmail.com>
2025-12-24 10:01:32 +0330

    add_ecomm_to_eQC

--- app/Http/Controllers/EcommQrCodeController.php
+++ app/Http/Controllers/EcommQrCodeController.php
@@ -13,7 +13,7 @@ class EcommQrCodeController extends Controller
 {
    
       public function load(ecomm $ecomm, string $slug){
-        return view('client.ecomm.ecomm_menu', ['career' => $career, 'slug' => $slug]);
+        return view('client.ecomm.ecomm_menu', ['ecomm' => $ecomm, 'slug' => $slug]);
 
       }
 }
