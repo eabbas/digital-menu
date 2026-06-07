fc7882159fd4b09248f94fe5ce291bec3281aa09
servin-dev <servinnasiri4979@gmail.com>
2026-06-07 18:41:13 +0330

    pageFaq debugged

--- resources/views/admin/pages/single.blade.php
+++ resources/views/admin/pages/single.blade.php
@@ -410,8 +410,7 @@
                                         <div class="mt-3 relative flex flex-col lg:flex-row gap-3">
                                             <div class="w-full lg:w-8/9 faqBox">
                                                 <div class="flex flex-row justify-between items-center gap-3 py-3 px-4 border-1 border-gray-400 bg-fuchsia-100 rounded-full relative transition-all duration-300">
-                                                    <div class="flex flex-row items-center gap-2"
-                                                         onclick='editFaq("{{ $FAQ->id }}")'>
+                                                    <div class="flex flex-row items-center gap-2">
                                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"
                                                              class="size-5 rounded-md">
                                                             <path d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
@@ -635,7 +634,7 @@
                                         </div>
                                     </div>
                                     {{-- FAQ --}}
-                                    {{-- <div class="w-full">
+                                    <div class="w-full">
                                         <div onclick="addFaq() "
                                              class="w-full p-5 border-1 border-gray-400 flex flex-row items-center gap-5 hover:ring-2 hover:ring-[#eb3254] hover:ring-offset-2 hover:bg-[#eb3254]/20 rounded-lg transition-all duration-150 cursor-pointer">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"
@@ -645,7 +644,7 @@
                                             </svg>
                                             <span class="text-gray-700 font-bold">افزودن سوالات متداول</span>
                                         </div>
-                                    </div> --}}
+                                    </div>
                                     {{-- FAQ end --}}
                                     {{-- page contactus --}}
                                     <div class="w-full">
@@ -2860,37 +2859,46 @@
                         let mainDiv = document.createElement('div')
                         mainDiv.className = "lg:py-2"
                         let element = `
-                    <h3 class="hidden lg:block text-lg font-bold text-gray-800 text-center cursor-pointer viewFAQ"
+                    <h3 class="lg:block text-lg font-bold text-gray-800 text-center cursor-pointer viewFAQ"
                         onclick='editTitle("${response.block.id}")'
                         data-block-id="${response.block.id}">
                         ${response.block.title}
                     </h3>
+
+
+
                     <div class="flex flex-row-reverse items-center gap-2">
                         <div class="p-1.5 rounded-md bg-blue-500 hover:bg-blue-600 cursor-pointer"
-                            onclick='addQuestionToBlock("${response.block.id}", this)'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 448 512">
-                                <path fill="white" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+                                onclick='addQuestionToBlock("${response.block.id}", this)'>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="size-4"
+                                    viewBox="0 0 448 512">
+                                <path fill="white"
+                                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
                             </svg>
                         </div>
-
-                    </div>                        <div class="p-1.5 rounded-md bg-green-500 hover:bg-green-600 cursor-pointer"
-                            onclick='editTitle("${response.block.id}", this)'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 512 512">
-                                <path fill="white" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/>
+                        <div class="p-1.5 rounded-md bg-green-500 hover:bg-green-600 cursor-pointer"
+                                onclick='editTitle("${response.block.id}", this)'>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="size-4"
+                                    viewBox="0 0 512 512">
+                                <path fill="white"
+                                        d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/>
                             </svg>
                         </div>
                         <div class="p-1.5 rounded-md bg-red-500 hover:bg-red-600 cursor-pointer"
-                            onclick='deleteTitle("${response.block.id}",this)'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 448 512">
-                                <path fill="white" d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
+                                onclick='deleteTitle("${response.block.id}",this)'>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="size-4"
+                                    viewBox="0 0 448 512">
+                                <path fill="white"
+                                        d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
                             </svg>
-                        </div>`;
+                        </div>
+                    </div>`
                         response.faqs.forEach((data) => {
                             element += `
                         <div class="mt-3 relative flex flex-col lg:flex-row gap-3">
                             <div class="w-full lg:w-8/9 faqBox">
                                 <div class="flex flex-row justify-between items-center gap-3 py-3 px-4 border-1 border-gray-400 bg-fuchsia-100 rounded-full relative transition-all duration-300">
-                                    <div class="flex flex-row items-center gap-2" onclick='editFaq("${data.id}")'>
+                                    <div class="flex flex-row items-center gap-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="size-5 rounded-md">
                                             <path d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
                                         </svg>
@@ -3251,7 +3259,7 @@
                 <div class="mt-3 relative flex flex-col lg:flex-row gap-3">
                     <div class="w-full lg:w-8/9 faqBox">
                         <div class="flex flex-row justify-between items-center gap-3 py-3 px-4 border-1 border-gray-400 bg-fuchsia-100 rounded-full relative transition-all duration-300">
-                            <div class="flex flex-row items-center gap-2" onclick='editFaq("${data.id}")'>
+                            <div class="flex flex-row items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="size-5 rounded-md">
                                     <path d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
                                 </svg>
@@ -3859,7 +3867,7 @@
             editFaqForm.classList.remove('invisible')
             editFaqForm.classList.remove('opacity-0')
             editFaqForm.classList.remove('top-full')
-            editFaqForm.classList.add('top-[70vh]')
+            editFaqForm.classList.add('top-[30vh]')
             editFaqForm.classList.add('-translate-y-1/7')
         }
 
@@ -4355,7 +4363,7 @@
                         <div class="mt-3 relative flex flex-col lg:flex-row gap-3">
                             <div class="w-full lg:w-8/9 contactusBox">
                                 <div class="flex flex-row justify-between items-center gap-3 py-3 px-4 border-1 border-gray-400 bg-fuchsia-100 rounded-full relative transition-all duration-300">
-                                    <div class="flex flex-row items-center gap-2" onclick='editFaq("${data.id}")'>
+                                    <div class="flex flex-row items-center gap-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="size-5 rounded-md">
                                             <path d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
                                         </svg>
